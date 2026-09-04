cf4f828f0f8698be6a8141f9ec2fd0903ff3136e
abhishekhkamble <akamble9913@gmail.com>
2026-09-04 12:07:45 +0530

    Test CI failure

--- test/server.test.js
+++ test/server.test.js
@@ -6,7 +6,7 @@ describe("API Tests", () => {
     const response = await request(app).get("/");
 
     expect(response.statusCode).toBe(200);
-    expect(response.body.message).toBe("Hello from DevOps!");
+    expect(response.body.message).toBe("Hello from cicd!");
   });
 
   test("GET /health should return OK", async () => {
